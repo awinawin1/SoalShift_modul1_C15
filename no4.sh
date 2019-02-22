be22473d39d0acdafe2a4a11669c95a7fa88c454
awinawin1 <43516997+awinawin1@users.noreply.github.com>
2019-02-22 12:18:49 +0700

    Create no4.sh

---
+++ no4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+hour=$(date +"%H")
+
+if [[ ${hour:0:1} -eq 0 ]]
+then
+    hour=${hour:1:1}
+fi
+
+#echo $hour
+
+lowcase=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+syslog=$(</var/log/syslog)
+#echo "$syslog"
+syslog=$(echo "$syslog" | tr "${lowcase:0:26}" "${lowcase:${hour}:26}")
+#echo "$syslog"
+
+upcase=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
+syslog=$(echo "$syslog" | tr "${upcase:0:26}" "${upcase:${hour}:26}")
+#echo "$syslog"
+
+thishour=$(date +"%H:%M %d-%m-%Y")
+#echo "$thishour"
+#echo "$syslog" > /home/awin/praktikum1/sislog
+echo "$syslog" > "$thishour"
+
