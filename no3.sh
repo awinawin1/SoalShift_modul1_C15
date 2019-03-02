84b57e65d8764337d3f7a049aadaeaa80fd0f5f1
awinawin1 <43516997+awinawin1@users.noreply.github.com>
2019-03-02 15:44:17 +0700

    Update no3.sh

--- no3.sh
+++ no3.sh
@@ -1,28 +1,42 @@
-
-#NOMOR 3
-
-
 #!/bin/bash
 
-loop=1
-ke=1
+uuid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 
+# echo $uuid
 
+file="password"
+ext=".txt"
 
-while [ $loop -ne 0 ]
+counter=1
+flag=1
+
+for passtxt in password/password*.txt
 do
+    path="password/password$counter.txt"
+    #echo "${passtxt:17:1}"
+    if [[ "*" == ${passtxt:17:1} ]]
+    then
+        echo "Still empty, so create new file password$counter.txt"
+        echo "$uuid" > $path
+        flag=0
+        break
+    elif [[ counter -ne ${passtxt:17:1} ]]
+    then
+        echo "password$counter.txt not available, create new file"
+        echo "$uuid" > $path
+        flag=0
+        break
+    else
+        echo "password$counter.txt available"
+    fi
+  counter=$((counter + 1))
+done
 
-if [[ -f /home/awin/praktikum1/password$ke.txt ]] ; then
-
-    ke=$((ke + 1))
-
-else
-
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1 > /home/awin/praktikum1/password$ke.txt
-loop=0
-
+if [[ flag -eq 1 ]]
+then
+    path="password/password$counter.txt"
+    echo "password$counter.txt not available, create new file"
+    echo "$uuid" > $path
 fi
 
 
-
-done
